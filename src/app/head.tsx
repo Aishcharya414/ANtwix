0cb2dc28494da49a48b7d9053bf02522bec99591
NHNm17 <it23318434@my.sliit.lk>
2026-05-12 23:07:44 +0530

    add forse head

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/logo-removebg.png" sizes="any" type="image/png" />
+      <link rel="shortcut icon" href="/logo-removebg.png" />
+      <link rel="apple-touch-icon" href="/logo-removebg.png" />
+      <meta name="theme-color" content="#000000" />
+    </>
+  );
+}
