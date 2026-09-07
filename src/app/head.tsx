74c33fb692d509ce19ade82353869299466e7a3e
NHNm17 <hiranmendi@gmail.com>
2026-09-07 21:57:11 +0530

    add

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -5,6 +5,7 @@ export default function Head() {
       <link rel="shortcut icon" href="/logo-removebg.png" />
       <link rel="apple-touch-icon" href="/logo-removebg.png" />
       <meta name="theme-color" content="#000000" />
+      <meta name="google-adsense-account" content="ca-pub-5596708780281669" />
     </>
   );
 }
